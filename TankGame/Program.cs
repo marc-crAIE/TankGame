68610319b5bb691bb10cf8a9268d48398fab50e3
marc-crAIE <marc.cookerussell.aie@gmail.com>
2023-05-24 20:18:09 +1000

    Code

--- TankGame/Program.cs
+++ TankGame/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace TankGame
+﻿using RayEngine.Core;
+using RayEngine.Debug;
+
+namespace TankGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ApplicationSpecification spec = new ApplicationSpecification("Sandbox App");
+
+            Profiler.BeginSession("Startup", "startup.json");
+
+            TankGame game = new TankGame(spec);
+
+            Profiler.EndSession();
+
+            Profiler.BeginSession("Runtime", "runtime.json");
+
+            game.Run();
+
+            Profiler.EndSession();
         }
     }
 }
---
+++ TankGame/TankGame.cs
@@ -0,0 +1,25 @@
+﻿using RayEngine.Core;
+using RayEngine.GameObjects;
+using RayEngine.Scenes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankGame
+{
+    internal class TankGame : Application
+    {
+        private Scene Scene;
+        private GameObject TankBody;
+
+        public TankGame(ApplicationSpecification specification) : base(specification)
+        {
+            Scene = new Scene();
+            SceneManager.LoadScene(Scene);
+
+            TankBody = new GameObject();
+        }
+    }
+}
